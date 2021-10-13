42aaa4b60227692510ce04e37ec27ddc3eb5bb58
Vladyslav30092021 <vvch2011@gmail.com>
2021-10-13 17:33:12 +0300

    some changes of style

--- client/components/dummy-view.jsx
+++ client/components/dummy-view.jsx
@@ -21,16 +21,28 @@ const Dummy = () => {
   return (
     <div>
       <Head title="Hello" />
+      <h1 className="text-grey-darkest">Program from 10-11 week</h1>
       <div>Enter your text: </div>
       <div className="text-black">
-        <input type="text" onChange={(e) => setText(e.target.value)} value={text} />
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
       </div>
       <div>
-        <button type="button" onClick={onClick}>
+        <button
+          type="button"
+          className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"
+          onClick={onClick}
+        >
           Send
         </button>
       </div>
-      <div>{textFromServer}</div>
+      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        {textFromServer}
+      </div>
     </div>
   )
 }
