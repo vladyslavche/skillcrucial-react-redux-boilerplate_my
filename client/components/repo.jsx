649b3fb700928c50164313aaf4ef24b13d3adf5f
VVCH <vvch@iMac-VVCH.local>
2021-10-27 21:18:27 +0300

    next with repo

--- client/components/repo.jsx
+++ client/components/repo.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
 import Head from './head'
 
 const Repo = () => {
   const { userName } = useParams()
+  const [repos, setRepos] = useState([])
+const url = `https://api.github.com/users/${userName}/repos`
+useEffect(() => {
+  fetch(url)
+  .then((r) => r.json())
+  .then((arr) => {
+    setRepos(arr)
+  })
+  .catch((err) => console.log('Error: ', err))
+}, [url])
   return (
     <div>
       <Head title="Repositories" />
       {userName}
-    </div>
+      {repos.map((repoObj) => {
+      return <Link to={`/${userName}/${repoObj.name}`} key={repoObj.id}>{repoObj.name}</Link>
+      })}
+      </div>
   )
 }
 
